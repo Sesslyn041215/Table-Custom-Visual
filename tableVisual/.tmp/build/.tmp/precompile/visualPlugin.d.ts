515a341dac623046d23be285944594f185de603b
Sesslyn041215 <sesslyn2004@gmail.com>
2024-06-30 22:24:06 +0530

    Completed

--- tableVisual/.tmp/build/.tmp/precompile/visualPlugin.d.ts
+++
@@ -1,4 +0,0 @@
-import powerbiVisualsApi from "powerbi-visuals-api";
-import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
-declare var tableVisual1120F870B56C4849818ABC5CCF83C9B8_DEBUG: IVisualPlugin;
-export default tableVisual1120F870B56C4849818ABC5CCF83C9B8_DEBUG;
